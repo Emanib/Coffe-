142d7c0dfa555fa8b8e0c122f17eb93e2b6505fe
eman ebrahim <adla48770@gmail.com>
2021-09-18 20:54:48 +0300

    slider

--- src/Fetch.js
+++ src/Fetch.js
@@ -1,12 +1,37 @@
+import styled from 'styled-components'
 
 import axios from 'axios'
-import { useState, useEffect } from 'react'
-// import Product from './Components/Product'
+import { useState, useEffect ,useRef} from 'react'
+import ProductSlider from './Components/Shop/Product'
+import Arroww from './images/Arroww.svg'
+import Arrow from './images/Arrow.svg'
+const Arrow1 = styled.img`
+/* width:100%;  */
+z-index:10;
+display:flex;
+ position:absolute; 
+top:50%;
+left:50px;
+
+`
+const Arrow2 = styled.img`
+/* width:100%;  */
+z-index:10;
+display:flex;
+ position:absolute; 
+top:50%;
+right:100px; 
+justify-content:space-between;
+
+`
 const Fetch = () =>
 {
    
     const url = process.env.REACT_APP_API_URL
   const [products, setProducts] = useState({ loading: false, data: null, error: false });
+  const [current, setCurrent] = useState(0)
+
+  const time = useRef(null)
   useEffect(() =>
   {
     setProducts({
@@ -33,14 +58,29 @@ const Fetch = () =>
   }
   if (products.data)
   {
-      content = products.data.map((product,key) => (
-        <div key = {product.id}> {product.price} </div>
+    var length = products.data.length
+      content = products.data.map((product,index) => (
+  current === index &&( <ProductSlider product = {product} key= {index} />)
          
-    ))
+    )) 
+
+  }
+   const nextSlide = () =>
+    {
+     setCurrent(current === length - 1 ? 0 : current + 1)
+     console.log(current)
+  }
+  const prevSlide = () =>
+  {
+    setCurrent(current === 0 ? length - 1 : current - 1)
+    console.log(current)
     }
+    
     return (
-   <div>
-    <div>  {content}  </div>    
+      <div style = {{display:"flex"}}>
+          <Arrow1 src={Arrow} alt='arrow' onClick = {prevSlide}  />
+       <div style={{ display: "flex" }}>  {content}  </div>
+           <Arrow2 src={Arroww} alt='arrow' onClick = { nextSlide}  />
 </div>
 
       );
